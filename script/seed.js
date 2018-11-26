36dfe586a4c15d4797042ff0bb8046ad5c11e66a
Lucas Levin <lucaslevin1@gmail.com>
2018-11-26 16:57:32 -0500

    updated seed file to include trades

--- script/seed.js
+++ script/seed.js
@@ -2,6 +2,7 @@
 
 const db = require('../server/db')
 const {User} = require('../server/db/models')
+const {Trade} = require('../server/db/models')
 
 async function seed() {
   await db.sync({force: true})
@@ -12,7 +13,23 @@ async function seed() {
     User.create({email: 'murphy@email.com', password: '123'})
   ])
 
+  const trades = await Promise.all([
+    Trade.create({
+      symbol: 'AAPL',
+      shares: 20,
+      price: 20.14,
+      userId: 1
+    }),
+    Trade.create({
+      symbol: 'GOOG',
+      shares: 40,
+      price: 30.14,
+      userId: 1
+    })
+  ])
+
   console.log(`seeded ${users.length} users`)
+  console.log(`seeded ${trades.length} trades`)
   console.log(`seeded successfully`)
 }
 
